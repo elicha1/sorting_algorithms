961c89f83c24a46d590f532738595b68b593a168
elicha1 <aicha95bouichou@gmail.com>
2023-12-19 11:16:40 -0800

    shel

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,48 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using
- *              the Shell sort algorithm.
- * @array: The array to be sorted.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending order using the shell
+ *		sort algorithm.
+ * @array: An array of integers.
  * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap, i, j;
-    int temp;
+	size_t gap, i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    /* Start with a large gap and reduce it in each iteration */
-    for (gap = size / 2; gap > 0; gap /= 2)
-    {
-        /* Perform gapped insertion sort for this gap size */
-        for (i = gap; i < size; i++)
-        {
-            /* Add a[i] to the elements that have been gap sorted */
-            /* Save a[i] in temp and make a hole at position i */
-            temp = array[i];
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-            /* Shift earlier gap-sorted elements up until the correct location for a[i] is found */
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-                array[j] = array[j - gap];
-
-            /* Put temp (the original a[i]) in its correct location */
-            array[j] = temp;
-        }
-        print_array(array, size);
-    }
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
-
